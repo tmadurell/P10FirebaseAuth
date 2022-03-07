dd9a4a37cccb0f53b12102bc7a5be536dc798037
tmadurell <amadurell@elpuig.xeill.net>
2022-03-07 15:58:10 +0100

    Authentication 6. SignIn con email/password

--- app/src/main/java/com/main/p10firebaseauth/SignInFragment.java
+++ app/src/main/java/com/main/p10firebaseauth/SignInFragment.java
@@ -6,11 +6,23 @@ import androidx.annotation.*;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.*;
 import android.view.*;
+import android.widget.*;
+
+import com.google.android.gms.tasks.*;
+import com.google.android.material.snackbar.*;
+import com.google.firebase.auth.*;
 
 
 public class SignInFragment extends Fragment {
 
     NavController navController;   // <-----------------
+    //6. SignIn con email/password
+    private EditText emailEditText, passwordEditText;
+    private Button emailSignInButton;
+    private LinearLayout signInForm;
+    private ProgressBar signInProgressBar;
+
+    private FirebaseAuth mAuth;
 
     public SignInFragment() {}
 
@@ -24,6 +36,14 @@ public class SignInFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         navController = Navigation.findNavController(view);
+        //6. SignIn con email/password
+        emailEditText = view.findViewById(R.id.emailEditText);
+        passwordEditText = view.findViewById(R.id.passwordEditText);
+        emailSignInButton = view.findViewById(R.id.emailSignInButton);
+        signInForm = view.findViewById(R.id.signInForm);
+        signInProgressBar = view.findViewById(R.id.signInProgressBar);
+
+
 
         view.findViewById(R.id.gotoCreateAccountTextView).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +51,40 @@ public class SignInFragment extends Fragment {
                 navController.navigate(R.id.registerFragment);
             }
         });
+        //6. SignIn con email/password
+        mAuth = FirebaseAuth.getInstance();
+
+            emailSignInButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    accederConEmail();
+                }
+            });
+        }
+
+    private void accederConEmail() {
+        signInForm.setVisibility(View.GONE);
+        signInProgressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            actualizarUI(mAuth.getCurrentUser());
+                        } else {
+                            Snackbar.make(requireView(), "Error: " + task.getException(), Snackbar.LENGTH_LONG).show();
+                        }
+                        signInForm.setVisibility(View.VISIBLE);
+                        signInProgressBar.setVisibility(View.GONE);
+                    }
+                });
     }
+
+    private void actualizarUI(FirebaseUser currentUser) {
+        if(currentUser != null){
+            navController.navigate(R.id.homeFragment);
+        }
+    }
+    //Final de 6. SignIn con email/password
 }
